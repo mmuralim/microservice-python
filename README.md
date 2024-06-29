55090c55d5432adc805fe7b770ecae79151b41cb
vagrant <vagrant@vgr-spark-base64>
2024-06-29 06:04:14 +0000

    python microservice

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # microservice-python
 create microservice with python,pip,flask and deploy with help
+
+installing python
+create python vir env
+installing python vscode ext
+sample flash app
+jinja template for dynamic web pages
+pip to freeze python dependencies
+build docker image with dockerfile
+write docker compose
+write kubernetes manifest
+create helm chart
---
+++ src/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, render_template
+import socket
+
+app = Flask(__name__)
+# Function to fetch hostname and ip
+def fetchDetails():
+     hostname = socket.gethostname()
+     host_ip = socket.gethostbyname(hostname)
+#     print("Hostname: ",host_name)
+#     print("IP: ",host_ip)
+     return str(hostname), str(host_ip)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World! </p>"
+
+@app.route("/health")
+def health():
+     return jsonify(
+          status="UP"
+     )
+@app.route("/details")
+def details():
+     hostname, ip = fetchDetails()
+     return render_template('index.html', HOSTNAME=hostname, IP=ip)
+
+if __name__ == '__main__':
+      app.run(host='192.168.0.125', port=5000)
